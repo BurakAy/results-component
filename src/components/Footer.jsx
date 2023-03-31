759b1ae9f7fea3f4cf01fe84131e2023e845f570
Burak <14030652+BurakAy@users.noreply.github.com>
2023-03-30 19:39:03 -0500

    organizing files, removing boilerplate, creating Footer component and Components directory, creating Footer.css and Styles directory

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div class="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://github.com/BurakAy" rel="noreferrer" target="_blank">
+        Burak
+      </a>
+      .
+    </div>
+  );
+};
+
+export default Footer;
